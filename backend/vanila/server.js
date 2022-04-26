c046ad56de4928e0cbb8df310efc1e98233bd277
Ренат Хакимов <khakimov@grechka.digital>
2022-04-26 17:04:27 +0400

    Added vanila server

---
+++ backend/vanila/server.js
@@ -0,0 +1,36 @@
+const http = require('http')
+const { getProducts, getProduct, createProduct, updateProduct, deleteProduct } = require('./controllers/productController')
+
+const server = http.createServer((req, res) => {
+
+    //disable CORS
+    res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Request-Method', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+	res.setHeader('Access-Control-Allow-Headers', '*');
+
+    if(req.url === '/api/products' && req.method === 'GET') {
+        getProducts(req, res)
+    } else if(req.url.match(/\/api\/products\/\w+/) && req.method === 'GET') {
+        const id = req.url.split('/')[3]
+        getProduct(req, res, id)
+    } else if(req.url === '/api/products' && req.method === 'POST') {
+        createProduct(req, res)
+    } else if(req.url.match(/\/api\/products\/\w+/) && req.method === 'PUT') {
+        const id = req.url.split('/')[3]
+        updateProduct(req, res, id)
+    } else if(req.url.match(/\/api\/products\/\w+/) && req.method === 'DELETE') {
+        const id = req.url.split('/')[3]
+        deleteProduct(req, res, id)
+    } else {
+        res.writeHead(404, { 'Content-Type': 'application/json' })
+        res.end(JSON.stringify({ message: 'Route Not Found' }))
+    }
+
+})
+
+const PORT =  process.env.PORT || 5000
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+
+module.exports = server;
